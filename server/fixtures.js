b89597aab19451942f5ce5c5d598d33038da367d
Vitalie Maldur <maldur.vitalik@gmail.com>
2015-06-29 21:31:35 +0300

    Voting support

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,6 +1,5 @@
 if (Posts.find().count() === 0) {
-  var now = new Date().getTime();
-// create two users
+  var now = new Date().getTime();// create two users
   var tomId = Meteor.users.insert({
     profile: { name: 'Tom Coleman' }
   });
@@ -15,12 +14,15 @@ if (Posts.find().count() === 0) {
     author: sacha.profile.name,
     url: 'http://sachagreif.com/introducing-telescope/',
     submitted: new Date(now - 7 * 3600 * 1000),
-    commentsCount: 2
+    commentsCount: 2,
+    upvoters: [],
+    votes: 0
   });
   Comments.insert({
     postId: telescopeId,
     userId: tom._id,
-    author: tom.profile.name,submitted: new Date(now - 5 * 3600 * 1000),
+    author: tom.profile.name,
+    submitted: new Date(now - 5 * 3600 * 1000),
     body: 'Interesting project Sacha, can I get involved?'
   });
   Comments.insert({
@@ -36,25 +38,29 @@ if (Posts.find().count() === 0) {
     author: tom.profile.name,
     url: 'http://meteor.com',
     submitted: new Date(now - 10 * 3600 * 1000),
-    commentsCount: 0
+    commentsCount: 0,
+    upvoters: [],
+    votes: 0
   });
   Posts.insert({
-    title: 'The Meteor Book',
-    userId: tom._id,
+    title: 'The Meteor Book',userId: tom._id,
     author: tom.profile.name,
     url: 'http://themeteorbook.com',
     submitted: new Date(now - 12 * 3600 * 1000),
-    commentsCount: 0
+    commentsCount: 0,
+    upvoters: [],
+    votes: 0
   });
-
   for (var i = 0; i < 10; i++) {
     Posts.insert({
       title: 'Test post #' + i,
       author: sacha.profile.name,
       userId: sacha._id,
       url: 'http://google.com/?q=test-' + i,
-      submitted: new Date(now - i * 3600 * 1000),
-      commentsCount: 0
+      submitted: new Date(now - i * 3600 * 1000 + 1),
+      commentsCount: 0,
+      upvoters: [],
+      votes: 0
     });
   }
 }
